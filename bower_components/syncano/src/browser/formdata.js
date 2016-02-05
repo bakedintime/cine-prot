4611fc71607afd5730bb81652da0eb8c41541126
Joel <jcantoral@tir.com.gt>
2016-02-05 11:59:30 -0600

    Agregar libreria para un servicio de database as a service, syncano

---
+++ bower_components/syncano/src/browser/formdata.js
@@ -0,0 +1,36 @@
+function DummyFormData() {
+    this.fake = true;
+    this.boundary = "------SyncanoFormBoundary" + Math.random().toString(36).substring(7);
+    this._fields = [];
+}
+
+DummyFormData.prototype.append = function(key, value, filename) {
+    this._fields.push([key, value, filename]);
+}
+
+DummyFormData.prototype.toString = function() {
+    var boundary = this.boundary;
+    var body = "";
+
+    this._fields.forEach(function(field) {
+        var key = field[0];
+        var value = field[1];
+
+        body += "--" + boundary + "\r\n";
+
+        // file upload
+        if (value instanceof File) {
+            body += "Content-Disposition: form-data; name=\"" + key + "\"; filename=\"" + value.name + "\"\r\n";
+            body += "Content-Type: " + value.type + "\r\n\r\n";
+            body += value.getAsBinary() + "\r\n";
+        } else {
+            body += "Content-Disposition: form-data; name=\"" + key + "\";\r\n\r\n";
+            body += value + "\r\n";
+        }
+    });
+
+    body += "--" + boundary +"--";
+    return body;
+}
+
+module.exports = window.FormData || DummyFormData;
